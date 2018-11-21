49f2f1afb1d3322aeead54b5ff053fbc0f481e4c
Andy Ettisberger <andy@ettisberger.ch>
2018-11-21 20:27:55 +0100

    Adjust viewport

--- src/server/index.js
+++ src/server/index.js
@@ -54,7 +54,7 @@ const mailConfig = {
 app.use('/', express.static('dist/'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../dist/index.html'));
+    res.sendFile(path.join(__dirname, './../dist/index.html'));
 });
 
 app.post('/api/tickets/send', function (req, res) {
