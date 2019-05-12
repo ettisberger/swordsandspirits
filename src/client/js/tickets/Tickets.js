34dcdd918063bc7a7c1fc0966ea03c38398357eb
Andy <andy@ettisberger.ch>
2019-05-12 11:34:41 +0200

    Update Tickets.js
    Change banner text

--- src/client/js/tickets/Tickets.js
+++ src/client/js/tickets/Tickets.js
@@ -118,7 +118,7 @@ class Tickets extends Component {
                 <Section>
                     <Inlay>
                         <SectionTitle>Tickets</SectionTitle>
-                        <Banner>Der offizielle Ticketverkauf ist ab sofort geschlossen. Es können jedoch weiterhin Tickets mit dem bestehenden Formular vorreserviert und gegen Barzahlung an der Abendkasse abgeholt werden. Die Abendkasse ist am Samstag ab 18.30 geöffnet,
+                        <Banner>Der offizielle Ticketverkauf ist ab sofort geschlossen. Die Abendkasse ist am Samstag ab 18.30 geöffnet,
                             am Sonntag ab 13 Uhr. Vielen Dank für Ihr Verständnis.</Banner>
                         <form noValidate autoComplete="on">
                             <Grid container spacing={24} justify={'center'}>
